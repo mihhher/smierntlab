1dbacd00ca26dc9e92c276d9910422dbf00e3976
mihhher <124463311+mihhher@users.noreply.github.com>
2023-02-11 21:12:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function randomPage() {
+  // Get a list of all the pages on the website
+  var pages = ['/page1', '/page2', '/page3'];
+  // Generate a random number between 0 and the number of pages
+  var randomNumber = Math.floor(Math.random() * pages
